91a5d57defd3ce8aa554ab9aeaf474f998b988f2
Jakub Biesiada <biesiadajakub@icloud.com>
2020-01-05 19:56:50 +0100

    Clean up

--- src/context.tsx
+++ src/context.tsx
@@ -10,10 +10,8 @@ export const State = React.createContext<{ [key: string]: any }>({});
 export const ProvideProviders = ({ providers, children }: Props) => {
   const state = {} as { [key: string]: any };
 
-  Object.entries(providers).map(provider => {
-    state[provider[0]] = provider[1].props
-      ? provider[1].props.value
-      : provider[1];
+  Object.entries(providers).map(([key, value]) => {
+    state[key] = value.props ? value.props.value : value;
   });
 
   return <State.Provider value={state}>{children}</State.Provider>;

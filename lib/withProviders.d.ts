61abc84f7f543902e413b67874988e78223350d0
Jakub Biesiada <biesiadajakub@icloud.com>
2020-03-12 09:21:37 +0100

    Update types

--- lib/withProviders.d.ts
+++ lib/withProviders.d.ts
@@ -1,2 +1,2 @@
 import React from 'react';
-export declare const withProviders: (...contexts: string[]) => (WrappedComponent: React.ComponentClass<{}, any>) => (props: any) => JSX.Element;
+export declare const withProviders: (...contexts: string[]) => (WrappedComponent: React.ComponentType<any>) => (props: any) => JSX.Element;
